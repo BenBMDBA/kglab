356591d9010e25bb018c8dd55f5a33a075a60d76
BenBMDBA <46876990+BenBMDBA@users.noreply.github.com>
2024-07-22 17:57:47 +1000

    Update subg.py

--- kglab/subg.py
+++ kglab/subg.py
@@ -503,7 +503,7 @@ optional style dictionary
         *,
         notebook: bool = False,
         select_menu=False,
-        cdn_resources='in_line'
+        cdn_resources='in_line',
         style: dict = None,
         ) -> pyvis.network.Network:
         """
@@ -519,7 +519,7 @@ optional style dictionary
     returns:
 a `PyVis` network object
         """
-        pyvis_graph = pyvis.network.Network(notebook=notebook,select_menu=select_menu,cdn_resources=cdn_resources)
+        pyvis_graph = pyvis.network.Network(notebook=notebook, select_menu=select_menu, cdn_resources=cdn_resources)
 
         if not style:
             style = {}
